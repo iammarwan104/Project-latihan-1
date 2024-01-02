1ec39390fb573dcc4443be4c3b69e898096cf709
Marwan <marwahpinrang6@gmail.com>
2024-01-02 23:25:32 +0800

    responsive-TeamSmith

--- src/latihan project 1/Main.jsx
+++ src/latihan project 1/Main.jsx
@@ -18,8 +18,8 @@ export default function () {
             {/* <OurClient /> */}
             {/* <Unsent /> */}
             {/* <Helping /> */}
-            <Design />
-            {/* <TeamSmith /> */}
+            {/* <Design /> */}
+            <TeamSmith />
             {/* <Marketing /> */}
         </>
     )
