b9ffb554d7d46b1b6221b7284d91c91ba5addf0b
Marwan <marwahpinrang6@gmail.com>
2024-01-04 23:39:50 +0800

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-search-bar/",
+  base: "/Project-latihan-1/",
   plugins: [react()]
 })
