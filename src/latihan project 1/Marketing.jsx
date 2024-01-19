2ec5c85544dfb291f05687c9e7c784c21244e4e6
Marwan <marwahpinrang6@gmail.com>
2024-01-19 13:51:50 +0800

    framer-motion-team-marketing

--- src/latihan project 1/Marketing.jsx
+++ src/latihan project 1/Marketing.jsx
@@ -1,20 +1,15 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import hero5 from './img/hero5.jpg'
 import hero6 from './img/hero6.jpg'
 import hero7 from './img/hero7.jpg'
+import { useInView } from 'framer-motion';
 export default function Marketing() {
-    const [showDesign, setShowDesign] = useState(false);
-    const [showDesignItems, setShowDesignItems] = useState(false);
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 2700) {
-        setShowDesign(true);
-        setShowDesignItems(true);
-      }
-    });
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once : true});
     return (
-        <section id='marketing' className={`relative ${
-            showDesignItems? "bottom-0 opacity-100" : "-bottom-52 opacity-0"
-          } duration-700 ease-in-out px-8 md:px-16 lg:px-32 pt-16 pb-24 bg-white`}>
+        <section id='marketing' ref={ref} className={`relative ${
+            isInView? "bottom-0 opacity-100" : "-bottom-52 opacity-0"
+          } duration-700 delay-[.4s] ease-in-out px-8 md:px-16 lg:px-32 pt-16 pb-24 bg-white`}>
             <div className='text-center text-sm mb-8'>
                 <h2 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold mb-2'>Caring is the new marketing</h2>
                 <p className='text-sm md:text-sm text-center lg:text-lg  mb-4 md:mb-8 lg:mb-12'>The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
@@ -22,7 +17,7 @@ export default function Marketing() {
             <div className='grid grid-cols-1 md:grid-cols-3 gap-16 md:gap-4'>
 
 
-                <div className={` relative ${showDesignItems ? 'top-0 opacity-1' : 'relative top-52 opacity-0'} duration-700 delay-100 ease-in-out`}>
+                <div className={` relative ${isInView ? 'top-0 opacity-1' : 'relative top-52 opacity-0'} duration-700 delay-[.5s] ease-in-out`}>
                     <img src={hero5} alt="..." width={300} height={300} loading='lazy' fetchpriofiry='low' className='h-full w-auto object-cover rounded-lg' />
                     <div className='absolute w-[80%] left-[50%] -translate-x-[50%] translate-y-[50%] -bottom-[0%] p-2 bg-slate-100 rounded-lg '>
                         <p className="text-center text-sm xl:text-lg">Creating Streamlined Safeguarding Processes with OneRen</p>
@@ -38,7 +33,7 @@ export default function Marketing() {
                 </div>
 
 
-                <div className={` relative ${showDesignItems ? 'top-0 opacity-1' : 'relative top-52 opacity-0'} duration-700 delay-200 ease-in-out`}>
+                <div className={` relative ${isInView ? 'top-0 opacity-1' : 'relative top-52 opacity-0'} duration-700 delay-[.6s] ease-in-out`}>
                     <img src={hero6} alt="..." width={300} height={300} fetchpriority="low" loading="lazy" className=' w-full h-auto rounded-lg' />
                     <div className='absolute w-[80%] left-[50%] -translate-x-[50%] translate-y-[50%] -bottom-[0%] p-2 bg-slate-100 rounded-lg '>
                         <p className="text-center text-sm xl:text-lg">Creating Streamlined Safeguarding Processes with OneRen</p>
@@ -54,7 +49,7 @@ export default function Marketing() {
                 </div>
 
 
-                <div className={` relative ${showDesignItems ? 'top-0 opacity-1' : 'relative top-52 opacity-0'} duration-700 delay-300 ease-in-out`}>
+                <div className={` relative ${isInView ? 'top-0 opacity-1' : 'relative top-52 opacity-0'} duration-700 delay-[.7s] ease-in-out`}>
                     <img src={hero7} alt="..." width={300} height={300} fetchpriority="low" loading="lazy" className=' w-full h-auto rounded-lg' />
                     <div className='absolute w-[80%] left-[50%] -translate-x-[50%] translate-y-[50%] -bottom-[0%] p-2 bg-slate-100 rounded-lg '>
                         <p className="text-center text-sm xl:text-lg">Creating Streamlined Safeguarding Processes with OneRen</p>
